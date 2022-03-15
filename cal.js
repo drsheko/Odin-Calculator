15302254e918fb33d717bb317c94f1c310a1c537
Shady Israel <drsheko91@gmail.com>
2022-03-15 02:16:10 -0500

    create operator functions and calculator design

---
+++ cal.js
@@ -0,0 +1,27 @@
+let a = ;
+let b = ;
+
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function power(a,b){
+    return Math.pow(a,b)
+}
+
+function getReminder(a,b){
+    return a%b
+}
+
