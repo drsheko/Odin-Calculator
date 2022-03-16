f1c5f8e3204746a85556b675ed478b14f03bd1aa
Shady Israel <drsheko91@gmail.com>
2022-03-16 04:44:50 -0500

    get first, second value and operator in js , add buttons classes and second display

--- cal.js
+++ cal.js
@@ -1,5 +1,5 @@
-let a = ;
-let b = ;
+let a = 0;
+let b = 0 ;
 
 function add(a,b){
     return a+b
@@ -24,4 +24,49 @@ function power(a,b){
 function getReminder(a,b){
     return a%b
 }
+
+
+function operate(operator,a,b){
+    if (operator==='+'){add(a,b)
+ }  if (operator==='-'){subtract(a,b)
+} if ( operator==='*'){multiply(a,b)
+}  if(operator==='/'){divide(a,b)
+
+}if(operator==="^"){power(a,b)
+
+}if(operator ==='%'){getReminder(a,b)}
+
+}
+
+const screen = document.querySelector('.display')
+screen.textContent='';
+
+const keys=document.querySelectorAll('.key')
+ keys.forEach(key=> 
+    key.addEventListener('click',(e)=>{
+        screen.textContent += e.target.value
+        firstValue=parseInt(screen.textContent)
+    }       
+       ))
+ 
+ const operationKeys= document.querySelectorAll('#operation')
+operationKeys.forEach((elem)=>{
+    elem.addEventListener('click',function(e){
+       let operator = e.target.value ;
+       screen.textContent+=e.target.value; 
+       getOtherValue()   
+    })
+})
+
+const lowerDisplay = document.querySelector('.lowerDisplay')
+lowerDisplay.textContent='';
+
+function getOtherValue(){
+  let secondValue =``;
+    keys.forEach(key=> 
+        key.addEventListener('click',(e)=>{
+            secondValue += e.target.value
+            
+          }))}
+    
 
