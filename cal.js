ea5f82f9c5604323e1efab03b6381d184207bbdf
Shady Israel <drsheko91@gmail.com>
2022-03-17 14:08:40 -0500

    fix add function

--- cal.js
+++ cal.js
@@ -53,11 +53,13 @@ function operate(a,b,operator){
         
         }   
         
- function add(a,b){return a+b}
+ function add(a,b){
+     let sum =parseFloat(a) +parseFloat(b);
+     return sum}
  function subtract(a,b){ return a-b }
 function multiply(a,b){return a*b}
 function divide(a,b){
-            if(b===0){return "Error" ; location.reload()}
+            if(b===0){return "Error" }
             else{return a/b}
             }
         
