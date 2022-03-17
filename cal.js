26a425bdfdec1b23d61f4039b1958af5c0edd621
Shady Israel <drsheko91@gmail.com>
2022-03-16 20:40:31 -0500

    add equal , clear , delete buttons

--- cal.js
+++ cal.js
@@ -2,9 +2,8 @@ let currentValue=``;
 let firstValue=``;
 let operator= ``;
 let secondValue=``;
-
-
-
+let finalResult=``;
+let currentDisplay;
 
 const screen = document.querySelector('.display')
 screen.textContent='';
@@ -17,7 +16,7 @@ const keys=document.querySelectorAll('.key')
     }       
        ))
  
- const operationKeys= document.querySelectorAll('#operation')
+const operationKeys= document.querySelectorAll('#operation')
 operationKeys.forEach((elem)=>{
     elem.addEventListener('click',function(e){
         firstValue +=currentValue
@@ -28,53 +27,62 @@ operationKeys.forEach((elem)=>{
 })
 
 const lowerDisplay = document.querySelector('.lowerDisplay')
-lowerDisplay.textContent='';
+lowerDisplay.textContent=0;
 
 function getOtherValue(){
   
     keys.forEach(key=> 
         key.addEventListener('click',(e)=>{
-            secondValue += e.target.value
-            
+            secondValue = e.target.value   
           }))}
 
+  const del = document.querySelector('#del')
+  del.addEventListener('click',(e)=>{
+  currentDisplay= screen.textContent
+  currentDisplay = currentDisplay.slice(0,-1);
+   screen.textContent=currentDisplay ;     
+          })  
 
 function operate(a,b,operator){
-
-  
-
-    
             if (operator==='+'){return add(a,b)
          }  if (operator==='-'){return subtract(a,b)
         } if ( operator==='*'){return multiply(a,b)
         }  if(operator==='/'){return divide(a,b)
-        
-        }if(operator==="^"){return power(a,b)
-        
+         }if(operator==="^"){return power(a,b)
         }if(operator ==='%'){ return getReminder(a,b)}
         
         }   
         
-
-        
-        function add(a,b){return a+b}
-        
-        function subtract(a,b){
-            return a-b
-        }
-        
-        function multiply(a,b){return a*b}
-        
-        function divide(a,b){
-            if(b===0){return "Error"}
+ function add(a,b){return a+b}
+ function subtract(a,b){ return a-b }
+function multiply(a,b){return a*b}
+function divide(a,b){
+            if(b===0){return "Error" ; location.reload()}
             else{return a/b}
-            
-        }
+            }
         
-        function power(a,b){return Math.pow(a,b)}
+ function power(a,b){return Math.pow(a,b)}
         
-        function getReminder(a,b){
-            return a%b
-        }
+  function getReminder(a,b){return a%b }
     
+ const equal = document.querySelector('#equal')
+ equal.addEventListener('click',function(e){
+    finalResult+=  operate(firstValue,secondValue,operator);
+    lowerDisplay.textContent= finalResult
+  })
 
+  /*operationKeys.forEach((ele)=>{
+    ele.addEventListener('click',(e)=>{
+        firstValue = finalResult;
+        operator= e.target.value;
+        secondValue= getOtherValue();
+        
+    })})*/
+
+
+  const clear = document.querySelector('#clear')
+  clear.addEventListener('click', (e)=>location.reload()) ;
+
+  
+  
+ 
